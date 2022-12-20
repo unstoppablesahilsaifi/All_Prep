9dfc269aa7c2c7bd1d3b6d2b606b0812b3deaf99
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-12-20 19:12:22 +0530

    Update ProblemSolving.cpp

--- ProblemSolving.cpp
+++ ProblemSolving.cpp
@@ -1,1 +1,25 @@
+// Bit Manipulation
 
+
+//SUM vs XOr
+long sumXor(long n) {
+
+
+
+long count=0;
+
+for(long i=0;i<=n;i++){
+
+   
+
+    if((i+n) == (i^n)){
+
+        count++;
+
+    }
+
+}
+
+return count;
+
+}
