0c897af834657f151ccaceb73906baac0e3f9eab
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2022-12-21 17:05:33 +0530

    Update SpringMVC.java

--- SpringMVC.java
+++ SpringMVC.java
@@ -1,1 +1,2 @@
-// Spring MVC is bascially module of spring by which we can create the java web application.
+// Spring MVC is bascially module(sub framework) of spring by which we can create the java web application.
+// Its is build on servlet API 
