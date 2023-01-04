405eda36bc670a2adbf6ee78aaf550ebbedc1863
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2023-01-04 21:24:33 +0530

    Update SpringMVC.java

--- SpringMVC.java
+++ SpringMVC.java
@@ -3,3 +3,13 @@
 
 Group id- used to identify your projects uniquely
 Artifact id- projects name
+
+
+String q= "CREATE TABLE BOOKS " +
+                   "(id INTEGER not NULL AUTO_INCREMENT, " +
+                   " book_name VARCHAR(255) not NULL, " + 
+                   " description VARCHAR(255) not NULL, " + 
+                   " author_name VARCHAR(255) not NULL, " +
+                   " category VARCHAR(255) not NULL, " +
+                   " PRIMARY KEY ( id ),"+
+                   " unique index book_name_unique (book_name ASC)visible)";
