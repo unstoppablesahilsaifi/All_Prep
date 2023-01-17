a3cbbbe569ba07f121421d373a3e3f4ed0173cb8
unstoppablesahilsaifi <97735834+unstoppablesahilsaifi@users.noreply.github.com>
2023-01-17 19:38:40 +0530

    Update ProblemSolving.cpp

--- ProblemSolving.cpp
+++ ProblemSolving.cpp
@@ -25,6 +25,34 @@ return count;
 }
 
 
-kk
-jjll
-kk
+
+
+// int main()
+// {
+//     /*
+//     for n=4;
+
+//       1
+//       2 3
+//       3 4 5
+//       4 5 6 7
+//     */
+
+//     int n;
+//     cin >> n;
+//     int i = 1;
+//     while (i <= n)
+//     {
+//         int j = 1;
+//         int value=i;
+//         while (j <= i)
+//         {
+//             cout <<value<< " ";
+//             value++;
+//             j++;
+//         }
+//         cout << endl;
+//         i++;
+//     }
+//     return 0;
+// }
